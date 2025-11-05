4dacb7da66f90ef9b6531d772d1758578f36a375
AdrienneJ123 <jobsadrienne@gmail.com>
2025-11-05 02:35:53 -0500

    Completed landing page and newsletter interactivity

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 /* Add your JavaScript to this file */
+window.addEventListener('DOMContentLoaded', () =>{
+  const form =document.querySelector('.newsletter form');
+   const emailInput =document.querySelector('.newsletter input[type="email"]');
+  const messageDiv=document.querySelector('.message');
+   form.addEventListener('submit', (event) =>{
+    event.preventDefault(); 
+    const email =emailInput.value.trim();
+    if (email=== '') {
+      messageDiv.textContent='Please enter a valid email address.';
+    } else {
+      messageDiv.textContent=`Thank you! Your email address ${email} has been added to our mailing list!`;
+    }});});
